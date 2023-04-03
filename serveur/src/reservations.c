a5a1948e26d6146ec29c823e8841960da3af1a9e
Anis Nouri <111896615+anis-nouri@users.noreply.github.com>
2023-04-03 14:13:24 +0000

    consulter historique

--- serveur/src/reservations.c
+++ serveur/src/reservations.c
@@ -22,8 +22,30 @@ void afficher_histo(char* filename) {
 }
 
 
+void consulter_historique_agence(char* filename, int agence) {
+    FILE* fp;
+    struct reservation res;
+
+    fp = fopen(filename, "r");
+    if (fp == NULL) {
+        printf("Erreur lors de l'ouverture du fichier %s\n", filename);
+        return;
+    }
+
+    printf("Référence vol\tAgence\tTransaction\tValeur\tRésultat\n");
+    while (fscanf(fp, "%d %d %s %d %s", &res.ref_vol, &res.agence, res.transaction, &res.valeur, res.resultat) != EOF) {
+        if (res.agence == agence) {
+            printf("%d\t\t%d\t%s\t\t%d\t%s\n", res.ref_vol, res.agence, res.transaction, res.valeur, res.resultat);
+        }
+    }
+
+    fclose(fp);
+}
+
+
 int main(){
     // Affichage de l'historique complet
     afficher_histo("./BD/histo.txt");
+    consulter_historique_agence("./BD/histo.txt", 2);
     return 0;
 }
